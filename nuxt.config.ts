b7759b44e770bf82899ead19c1fd565ff983006d
FelipeFVdev <feliep8@gmail.com>
2024-04-10 00:39:34 -0300

    config/configurations

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "@vueuse/nuxt", "nuxt-icon"],
+
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@vueuse/nuxt",
+    "nuxt-icon",
+    "nuxt-vuefire",
+    "@morev/vue-transitions/nuxt",
+    "@vee-validate/nuxt",
+  ],
+
+  veeValidate: {
+    // disable or enable auto imports
+    autoImports: true,
+    // Use different names for components
+    componentNames: {
+      Form: "VeeForm",
+      Field: "VeeField",
+      FieldArray: "VeeFieldArray",
+      ErrorMessage: "VeeErrorMessage",
+    },
+  },
 
   tailwindcss: {
     exposeConfig: true,
@@ -12,6 +33,7 @@ export default defineNuxtConfig({
   },
 
   typescript: {
+    strict: true,
     shim: false,
   },
 
@@ -26,6 +48,27 @@ export default defineNuxtConfig({
         name: "VariantProps",
         type: true,
       },
+      {
+        from: "vue-sonner",
+        name: "toast",
+        as: "useSonner",
+      },
     ],
+  },
+
+  vuefire: {
+    auth: { enabled: true, sessionCookie: true },
+    config: {
+      apiKey: process.env.apiKey,
+      authDomain: process.env.authDomain,
+      projectId: process.env.projectId,
+      storageBucket: process.env.storageBucket,
+      messagingSenderId: process.env.messagingSenderId,
+      appId: process.env.appId,
+    },
+  },
+
+  build: {
+    transpile: ["vue-sonner"],
   },
 });
